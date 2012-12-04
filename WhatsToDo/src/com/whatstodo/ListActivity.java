3b9d2ba6a530df5cd3049d0a8e1b1e4cd0be1cd0
nikoBeuth <niko.harnge@beuth-hochschule.de>
2012-12-04 18:22:58 +0100

    test commit

--- WhatsToDo/src/com/whatstodo/ListActivity.java
+++ WhatsToDo/src/com/whatstodo/ListActivity.java
@@ -1,7 +1,7 @@
 package com.whatstodo;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class ListActivity extends Activity {
@@ -10,6 +10,9 @@ public class ListActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list);
+		
+		//test
+		
 	}
 
 	@Override

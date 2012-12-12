73860f130ad7f35c52bb25f143548c901b592401
alexBeuth <s47789@beuth-hochschule.de>
2012-12-12 15:10:52 +0100

    Format

--- WhatsToDo/src/com/whatstodo/ListContainerActivity.java
+++ WhatsToDo/src/com/whatstodo/ListContainerActivity.java
@@ -21,19 +21,19 @@ import android.widget.TextView;
 import com.whatstodo.list.List;
 import com.whatstodo.list.ListActivity;
 
-public class ListContainerActivity extends Activity implements OnClickListener{
-	
+public class ListContainerActivity extends Activity implements OnClickListener {
+
 	ListContainer container;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_listcontainer);
-		
+
 		Button createList = (Button) findViewById(R.id.newList);
 		createList.setOnClickListener(this);
-		
+
 		container = ListContainer.getInstance();
 		showLists();
 	}
@@ -44,76 +44,77 @@ public class ListContainerActivity extends Activity implements OnClickListener{
 		getMenuInflater().inflate(R.menu.activity_list, menu);
 		return true;
 	}
-	
+
 	// @Override
 	public void onClick(View view) {
-		
-		if(view.getId() == R.id.newList) {
+
+		if (view.getId() == R.id.newList) {
 			EditText editText = (EditText) findViewById(R.id.list);
 			container.addList(editText.getText().toString());
 			showLists();
-		}
-		else if (view instanceof Button){
-			
-		    String viewName = ((Button)view).getText().toString();
-		    			
-			for(List list : container.getLists()){
-				
-				if(viewName == list.getName()){
+		} else if (view instanceof Button) {
+
+			String viewName = ((Button) view).getText().toString();
+
+			for (List list : container.getLists()) {
+
+				if (viewName == list.getName()) {
 					Intent intent = new Intent(this, ListActivity.class);
 					Bundle bundle = new Bundle();
-					bundle.putLong("ListId", list.getId()); //List ID
-					intent.putExtras(bundle); //Put your id to your next Intent
+					bundle.putLong("ListId", list.getId()); // List ID
+					intent.putExtras(bundle); // Put your id to your next Intent
 					startActivity(intent);
-					//finish();					
+					// finish();
 				}
 			}
 		}
 	}
-	
-	
+
 	/**
-	 * Shows all existing lists in a linear layout.
-	 * The method gets the lists from the class List Container.
+	 * Shows all existing lists in a linear layout. The method gets the lists
+	 * from the class List Container.
 	 */
-	
+
 	private void showLists() {
-		
+
 		ArrayList<String> listNames = new ArrayList<String>();
-		
-		for (List list : container.getLists()){
+
+		for (List list : container.getLists()) {
 			listNames.add(list.getName());
-		}			
-	    ListView listList1 = (ListView)findViewById(R.id.list1);
-	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.listitem, listNames);
-	    listList1.setAdapter(adapter);
-	    registerForContextMenu(listList1);
+		}
+		ListView listList1 = (ListView) findViewById(R.id.list1);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				R.layout.listitem, listNames);
+		listList1.setAdapter(adapter);
+		registerForContextMenu(listList1);
 	}
-	
+
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v,
-	    ContextMenuInfo menuInfo) {
-	  if (v.getId()==R.id.list1) {
-	    AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
-	    List list = container.getList(info.id);
-	    menu.setHeaderTitle(list.getName());
-	    String[] menuItems = getResources().getStringArray(R.array.menu);
-	    for (int i = 0; i<menuItems.length; i++) {
-	      menu.add(Menu.NONE, i, i, menuItems[i]);
-	    }
-	  }
+			ContextMenuInfo menuInfo) {
+		if (v.getId() == R.id.list1) {
+			AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+			List list = container.getList(info.id);
+			menu.setHeaderTitle(list.getName());
+			String[] menuItems = getResources().getStringArray(R.array.menu);
+			for (int i = 0; i < menuItems.length; i++) {
+				menu.add(Menu.NONE, i, i, menuItems[i]);
+			}
+		}
 	}
 
 	@Override
 	public boolean onContextItemSelected(MenuItem item) {
-	  AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
-	  String[] menuItems = getResources().getStringArray(R.array.menu);
-	  String menuItemName = menuItems[item.getItemId()];
-	  List list = container.getList(info.id);
-	  String listItemName = list.getName();
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item
+				.getMenuInfo();
+		String[] menuItems = getResources().getStringArray(R.array.menu);
+		String menuItemName = menuItems[item.getItemId()];
+		List list = container.getList(info.id);
+		String listItemName = list.getName();
 
-	  TextView text = (TextView)findViewById(R.id.footer);
-	  text.setText(String.format("Selected %s for item %s", menuItemName, listItemName));
-	  return true;
+		TextView text = (TextView) findViewById(R.id.footer);
+		text.setText(String.format("Selected %s for item %s", menuItemName,
+				listItemName));
+		return true;
 	}
 }

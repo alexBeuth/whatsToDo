17b1f222ff5ec48b5a7d6071312fea66742930ce
nikoBeuth <niko.harnge@beuth-hochschule.de>
2012-12-14 13:28:12 +0100

    ArrayAdapter muss überarbeitet werden

---
+++ WhatsToDo/src/com/whatstodo/ListAdapter.java
@@ -0,0 +1,15 @@
+package com.whatstodo;
+
+import java.util.ArrayList;
+import com.whatstodo.list.List;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class ListAdapter extends ArrayAdapter<List>{
+	
+	public ListAdapter(Context ctx, int textViewResourceId,
+			ListContainer container) {
+		super(context, textViewResourceId, objects);
+	}
+}

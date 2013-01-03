9fe9c098c580cfcdb015cff93b7d245c54a8f9e5
alexBeuth <s47789@beuth-hochschule.de>
2013-01-03 19:33:53 +0100

    New List implementation

--- WhatsToDo/src/com/whatstodo/list/List.java
+++ WhatsToDo/src/com/whatstodo/list/List.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -17,24 +19,13 @@ import com.whatstodo.task.Task;
  * @author alex
  * 
  */
-public class List implements Serializable, Iterable<Task> {
-
+public class List implements Serializable, java.util.List<Task> {
 
 	private static final long serialVersionUID = 0;
-	
+
 	transient private int size;
-	transient private Node first;
-	
-	private class Node {
-
-		Node next;
-		Task task;
-		
-		public Node(Task task) {
-			this.task = task;
-		}
-	}
-
+	transient private Task[] orderedTasks; 
+	transient private Comparator<Task> comparator;
 	private long id;
 	private String name;
 
@@ -42,79 +33,13 @@ public class List implements Serializable, Iterable<Task> {
 		
 		id = ListContainer.getNextListId();
 		this.name = name + " (" + id + ")";
-		first = null;
-		size = 0;
+		orderedTasks = new Task[1];
 	}
-
-	/**
-	 * Is the list empty?
-	 */
-	public boolean isEmpty() {
-		return first == null;
-	}
-
-	/**
-	 * Return the number of items in the list.
-	 */
-	public int size() {
-		return size;
-	}
-
-	/**
-	 * Add the item to the list.
-	 */
-	public void push(Task item) {
-		Node oldfirst = first;
-		first = new Node(item);
-		first.next = oldfirst;
-		size++;
-	}
-
-	/**
-	 * Delete and return the item most recently added to the list.
-	 * 
-	 * @throws java.util.NoSuchElementException
-	 *             if stack is empty.
-	 */
-	public Task pop() {
-		if (isEmpty())
-			throw new NoSuchElementException("Stack underflow");
-		Task item = first.task; // save item to return
-		first = first.next; // delete first node
-		size--;
-		return item; // return the saved item
-	}
-
-	@Override
-	public Iterator<Task> iterator() {
-		return new ListIterator();
-	}
-
-	/**
-	 * An iterator to the stack that iterates through the items in LIFO order.
-	 * remove() is not implemented.
-	 * 
-	 * @author alex
-	 * 
-	 */
-	private class ListIterator implements Iterator<Task> {
-		private Node current = first;
-
-		public boolean hasNext() {
-			return current != null;
-		}
-
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
-
-		public Task next() {
-			if (!hasNext())
-				throw new NoSuchElementException();
-			Task item = current.task;
-			current = current.next;
-			return item;
-		}
+	
+	public List(String name, Comparator<Task> comparator) {
+		
+		this(name);
+		this.comparator = comparator;
 	}
 
 	public String getName() {
@@ -128,12 +53,241 @@ public class List implements Serializable, Iterable<Task> {
 	public long getId() {
 		return id;
 	}
-	
+
 	public void setId(long id) {
 		this.id = id;
 	}
 
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
 
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public Iterator<Task> iterator() {
+		return new ListIterator();
+	}
+
+	@Override
+	public void add(int location, Task task) {
+		// TODO Maybe call add(Task)
+		throw new UnsupportedOperationException("List.add(int, Task)");
+	}
+
+	@Override
+	public boolean add(Task task) {
+		// double size of array if necessary
+		if (size == orderedTasks.length - 1)
+			resize(2 * orderedTasks.length);
+
+		// Add item
+		int i = size - 1;
+		while (i >= 0 && greater(task, orderedTasks[i])) {
+			orderedTasks[i + 1] = orderedTasks[i];
+			i--;
+		}
+		orderedTasks[i + 1] = task;
+		size++;
+		return true;
+	}
+
+	@Override
+	public boolean addAll(int location, Collection<? extends Task> collection) {
+		// TODO Maybe call addAll(Collection)
+		throw new UnsupportedOperationException("List.addAll(int, Collection)");
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends Task> collection) {
+		for (Task newTask : collection) {
+			add(newTask);
+		}
+		return true;
+	}
+
+	@Override
+	public void clear() {
+		for (int i = 0; i < size; i++) {
+			orderedTasks[i] = null;
+		}
+		size = 0;
+	}
+
+	@Override
+	public boolean contains(Object object) {
+		return indexOf(object) > 0;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> collection) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Task get(int location) {
+		checkBound(location);
+		return orderedTasks[location];
+	}
+
+	@Override
+	public int indexOf(Object object) {
+
+		for (int i = 0; i < size; i++) {
+			if (orderedTasks[i].equals(object)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	@Override
+	public int lastIndexOf(Object object) {
+
+		for (int i = size; i >= 0; i++) {
+			if (orderedTasks[i].equals(object)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	@Override
+	public java.util.ListIterator<Task> listIterator() {
+		throw new UnsupportedOperationException("List.listIterator");
+	}
+
+	@Override
+	public java.util.ListIterator<Task> listIterator(int location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Task remove(int location) {
+
+		if (isEmpty())
+			throw new NoSuchElementException("Underflow");
+		Task toReturn = orderedTasks[location];
+
+		size--;
+		for (int i = location; i < size; i++) {
+			orderedTasks[i] = orderedTasks[i + 1];
+		}
+		orderedTasks[size + 1] = null; // avoid loitering and help with garbage
+		// collection
+
+		// resize array
+		if ((size > 0) && (size == (orderedTasks.length - 1) / 4))
+			resize(orderedTasks.length / 2);
+
+		return toReturn;
+
+	}
+
+	@Override
+	public boolean remove(Object object) {
+		int indexOf = indexOf(object);
+		if (indexOf >= 0) {
+			remove(indexOf);
+			return true;
+		} else
+			return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> collection) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> collection) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Task set(int location, Task object) {
+		throw new UnsupportedOperationException("List.set(int, Task)");
+	}
+
+	@Override
+	public java.util.List<Task> subList(int start, int end) {
+		throw new UnsupportedOperationException("List.subList(int, int)");
+	}
+
+	@Override
+	public Object[] toArray() {
+		return orderedTasks;
+	}
+
+	@Override
+	public <T> T[] toArray(T[] array) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private boolean greater(Task i, Task j) {
+		if (comparator == null) {
+			return i.compareTo(j) > 0;
+		} else {
+			return comparator.compare(i, j) > 0;
+		}
+	}
+
+	private void checkBound(int index) {
+		if (index < 0 || index > size)
+			throw new IndexOutOfBoundsException("In " + index + ", S" + size);
+	}
+
+	// helper function to double the size of the heap array
+	private void resize(int capacity) {
+		assert capacity > size;
+		Task[] temp = new Task[capacity];
+		for (int i = 0; i <= size; i++)
+			temp[i] = orderedTasks[i];
+		orderedTasks = temp;
+	}
+
+	/**
+	 * An iterator to the stack that iterates through the items in LIFO order.
+	 * remove() is not implemented.
+	 * 
+	 * @author alex
+	 * 
+	 */
+	private class ListIterator implements Iterator<Task> {
+
+		private int i;
+
+		public ListIterator() {
+			i = 0;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return i < size;
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public Task next() {
+			if (!hasNext())
+				throw new NoSuchElementException();
+
+			return orderedTasks[i++];
+		}
+	}
 
 	/**
 	 * Serializes this object to the given stream.
@@ -148,10 +302,8 @@ public class List implements Serializable, Iterable<Task> {
 	private void writeObject(ObjectOutputStream s) throws IOException {
 		s.defaultWriteObject();
 		s.writeInt(size);
-		Node e = first;
-		while (e != null) {
-			s.writeObject(e.task);
-			e = e.next;
+		for (int i = 0; i < orderedTasks.length; i++) {
+			s.writeObject(orderedTasks[i]);
 		}
 	}
 
@@ -171,12 +323,13 @@ public class List implements Serializable, Iterable<Task> {
 			ClassNotFoundException {
 		s.defaultReadObject();
 		int i = s.readInt();
+		orderedTasks = new Task[1];
 		while (--i >= 0)
-			push(((Task) s.readObject()));
+			add(((Task) s.readObject()));
 	}
-	
+
 	public void addTask(String name) {
-		push(new Task(name));
+		add(new Task(name));
 	}
-	
+
 }
--- WhatsToDo/src/com/whatstodo/task/Task.java
+++ WhatsToDo/src/com/whatstodo/task/Task.java
@@ -1,16 +1,18 @@
 package com.whatstodo.task;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import com.whatstodo.ListContainer;
 import com.whatstodo.utility.Priority;
 
-public class Task implements Serializable {
+public class Task implements Serializable, Comparable<Task> {
 
 	private static final long serialVersionUID = 0;
 	private long id;
 	private String name;
 	private Priority priority;
+	private Date date;
 	
 	public Task(String name) {
 		this.setName(name);
@@ -36,6 +38,29 @@ public class Task implements Serializable {
 
 	public void setPriority(Priority priority) {
 		this.priority = priority;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public int compareTo(Task another) {
+		
+		if(date == null && another.date == null)
+			return 0;
+		
+		if(date != null) {
+			if(another.date != null)
+				return date.compareTo(another.date);
+			else
+				return 1;
+		}
+		return -1;
 	}
 
 }
